4e36fa9b2485db9e737292e2527887a1dccefdeb
Ramon Silva Freitas <ramonsilvaf@gmail.com>
2024-08-28 17:11:02 -0300

    inserção de codigo do CRUD nos arquivos ProduttoControlller e Poduto

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\Produto;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\view\View;
+use Symfony\Contracts\Service\Attribute\Required;
 
 class ProdutoController extends Controller
 {
@@ -12,9 +18,13 @@ class ProdutoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(): view
     {
         //
+        $produtos = Produto::lastest()->paginate(5);
+
+        return view('produtos.index', compact('produtos'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
     }
 
     /**
@@ -22,9 +32,9 @@ class ProdutoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(): view
     {
-        //
+        return view('produtos.create');
     }
 
     /**
@@ -33,9 +43,19 @@ class ProdutoController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $request->validate([
+            'descricao' => 'required',
+            'qtd' => 'required',
+            'precoUnitario' => 'required',
+            'precoVenda' => 'required',
+        ]);
+
+        Produto::create($request->all());
+
+        return redirect()->route('produtos.index')
+            ->with('success', 'Produto criado com sucesso.');
     }
 
     /**
@@ -44,9 +64,9 @@ class ProdutoController extends Controller
      * @param  \App\Models\Produtos  $produtos
      * @return \Illuminate\Http\Response
      */
-    public function show(Produto $produto)
+    public function show(Produto $produto): view
     {
-        //
+        return view('produtos.show', compact('produto'));
     }
 
     /**
@@ -55,9 +75,9 @@ class ProdutoController extends Controller
      * @param  \App\Models\Produtos  $produtos
      * @return \Illuminate\Http\Response
      */
-    public function edit(Produto $produto)
+    public function edit(Produto $produto): view
     {
-        //
+        return view('produtos.edit', compact('produto'));
     }
 
     /**
@@ -67,9 +87,19 @@ class ProdutoController extends Controller
      * @param  \App\Models\Produtos  $produtos
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Produto $produto)
+    public function update(Request $request, Produto $produto): RedirectResponse
     {
-        //
+        $request->calidate([
+            'descricao' => 'required',
+            'qtd' => 'required',
+            'precoUnitario' => 'required',
+            'precoVenda' => 'required',
+        ]);
+
+        $produto->update($request->all());
+
+        return redirect()->route('produto.index')
+            ->with('success', 'Produto atualizado com sucesso.');
     }
 
     /**
@@ -78,8 +108,11 @@ class ProdutoController extends Controller
      * @param  \App\Models\Produtos  $produtos
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Produto $produto)
+    public function destroy(Produto $produto): RedirectResponse
     {
-        //
+        $produto->delete();
+
+        return redirect()->route('produto')
+            ->with('success', 'Produto excluido com sucesso.');
     }
 }
--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Produto extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'descricao',
+        'qtd',
+        'precoUnitario',
+        'precoVenda'
+    ];
 }
